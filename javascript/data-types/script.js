a27df21980b46fc9f82823e39bfb19ce155efffb
daniaadil <20it032dani@ug.cusat.ac.in>
2024-10-18 18:55:05 +0530

    Add data types project files

---
+++ javascript/data-types/script.js
@@ -0,0 +1,32 @@
+//!-- PRIMITIVE DATATYPE--
+// Number -Storing age
+const age =25;
+console.log(age);
+
+// String - Storing name
+const name = "DANIA";
+console.log(name);
+
+// Boolean - Is the user logged in?
+const isLoggedIn = true;
+console.log(isLoggedIn);
+
+//Undefined -  A variable that has been declared but not assigned a value.
+ let grade;
+ console.log(grade);
+
+ //Null - Represents no value or "nothing".
+ const income = null;
+ console.log(income);
+
+ //!-- NON-PRIMITIVE DATATYPE--
+ //Objects - Storing user data
+ const user = {
+    firstName:'dania',
+    age: 23,
+    grade: 'A',
+ };
+
+ //Array - Storing a list of numbers
+ const mixedData = [19, 'danz', true, {}, undefined];
+ console.log(mixedData);
