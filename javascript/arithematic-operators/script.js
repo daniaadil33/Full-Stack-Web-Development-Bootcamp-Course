eafbd2d928ae67ccee36d579bd356bb4d6a64ee9
daniaadil <20it032dani@ug.cusat.ac.in>
2024-10-19 15:21:31 +0530

    Add arithematic operators

---
+++ javascript/arithematic-operators/script.js
@@ -0,0 +1,30 @@
+//! BASIC ARITHEMATIC OPERATIONS
+let a =5;
+let b =2;
+//! Addition
+//const result = a + b;
+
+//! Subtraction
+//const result = a - b;
+
+//! Multiplication
+//const result = a * b;
+
+//! Division
+//const result = a/b;
+
+//! Modulus to find remainder
+//const result= a % b;
+// console.log(result);
+//! Increment and Decrement
+//let number = 10;
+//number ++;
+//number --;
+
+//console.log(number);
+
+//! Compound Assignemnt with Arithematic Operators
+let score = 5;
+score += 2;
+console.log(score);
+
