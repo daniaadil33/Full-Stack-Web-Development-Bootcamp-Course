5957f8e34690bec5fb9772bf2be0f986257f297c
daniaadil <20it032dani@ug.cusat.ac.in>
2024-10-19 15:42:21 +0530

    Add comparison operators

---
+++ javascript/comparison-operators/script.js
@@ -0,0 +1,20 @@
+let ageOfDania = 23;
+let ageOfDijja = 23;
+let date = '23';
+let ageOfLaali= 10;
+
+//! using ==
+//console.log(ageOfDania == ageOfDijja);
+//console.log(ageOfDania == date);
+
+//! using ===
+//console.log(ageOfDania === date);  // returns false. coz it considers both value and tpe
+
+//! using !=
+//console.log(ageOfDania != ageOfLaali);
+
+//! using !==
+//console.log(ageOfDania !== date);
+
+//! using >
+console.log(ageOfDania == ageOfDijja);
