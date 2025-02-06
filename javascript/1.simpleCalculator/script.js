515a7690e36252849aad70b69b48e98e83c6fed5
daniaadil33 <115731157+daniaadil33@users.noreply.github.com>
2025-02-06 12:23:56 +0530

    Build a simple calculator

---
+++ javascript/1.simpleCalculator/script.js
@@ -0,0 +1,36 @@
+//const add = function add(a,b){
+// let results = a + b ;
+//  return results;
+//}
+
+//console.log(add(10,2));
+//ADDITION
+/*const add = (a,b) => {
+  let results = a + b ;
+  return results;
+};
+
+//console.log(add(10,2)) 
+
+
+//!SUBTRACTION
+/*const subtract = (m,n)=>{
+    let results = m - n;
+    return results;
+};
+subtract(20,10); */
+
+/*
+//!MULTIPLY
+const multiply = (m,n)=>{
+  let results = m * n;
+  return results;
+}
+console.log(multiply(20,10)); */
+
+//!DIVISON
+const division = (m,n)=>{
+  let results = m/n;
+  return results;
+}
+console.log(division(20,10));
