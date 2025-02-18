55c61c19b289bf0d8b3e8aa1c293e082529e1613
daniaadil33 <115731157+daniaadil33@users.noreply.github.com>
2025-02-18 10:44:22 +0530

    DOM

---
+++ javascript/DOM/script.js
@@ -0,0 +1,16 @@
+//----
+//WINDOW Object
+//-----
+//console.log(window.document);
+//console.log(window.innerHeight);
+//console.log(window.location);
+
+//console.log(window.location.host);
+//console.log(window.history);
+//console.log(window.navigator);
+//console.log(window.screen);
+//console.log(window.alert());
+//console.log(window.confirm());
+//window.prompt();
+//window.open('https://www.udemy.com/course/fullstack-web-development-course-projects-base/learn/lecture/41148324#overview');
+console.log(document.body);
