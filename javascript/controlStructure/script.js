a6764df6233ed6f5eb3ce197000a20525b5f60ae
daniaadil33 <115731157+daniaadil33@users.noreply.github.com>
2025-02-26 12:00:31 +0530

    commit control structures

---
+++ javascript/controlStructure/script.js
@@ -0,0 +1,9 @@
+let age = 10;
+
+if(age>= 16){
+    console.log("you are eligible to drive");
+}else if (age>= 18){
+    console.log("you are eligible to vote");
+}else {
+    console.log("you are too young to vote and drive");
+}
